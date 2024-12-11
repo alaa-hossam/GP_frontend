bbf7895422373cd4731b4eb6a091e2ad5410b955
MaryamSakr <102592232+MaryamSakr@users.noreply.github.com>
2024-12-11 19:34:51 +0200

    Advertisements Models

---
+++ lib/Models/AdvertisementModel.dart
@@ -0,0 +1,15 @@
+class AdvertisementsModel {
+   String image;
+   String link;
+   
+
+   AdvertisementsModel({
+      required this.image,
+      required this.link});
+
+
+
+}
+
+
+
---
+++ lib/ViewModels/AdvertisementsViewModel.dart
@@ -0,0 +1,10 @@
+import '../Models/AdvertisementModel.dart';
+
+class AdvertisementsViewModel {
+  List<AdvertisementsModel> Advertisements;
+  AdvertisementsViewModel({
+    required this.Advertisements
+});
+}
+
+
