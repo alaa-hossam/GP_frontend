257a9f6868c39aa23de7fe02d0638553e15a1541
alaa-hossam <alaa.hossam.2013@gmail.com>
2025-06-26 17:39:16 +0300

    Create handicrafterProfileClientView.dart

---
+++ lib/views/handicrafterProfileClientView.dart
@@ -0,0 +1,351 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:gp_frontend/Models/handcrafterModel.dart';
+import 'package:gp_frontend/ViewModels/handcrafterViewModel.dart';
+import 'package:gp_frontend/widgets/Dimensions.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import '../Providers/ProductProvider.dart';
+import '../widgets/customProduct.dart';
+import '../widgets/customizeButton.dart';
+import 'addCrafterReel.dart';
+
+class HandcrafterProfileClientView extends StatefulWidget {
+  static String id = "HandcrafterProfileClientViewScreen";
+  const HandcrafterProfileClientView({super.key});
+
+  @override
+  State<HandcrafterProfileClientView> createState() => _HandcrafterProfileClientViewState();
+}
+
+class _HandcrafterProfileClientViewState extends State<HandcrafterProfileClientView> {
+  File? _image;
+  handcrafterViewModel hvm = handcrafterViewModel();
+  handcrafterModel? _handcrafter;
+  bool _isLoading = true;
+  late productProvider prodProvider;
+
+  Future<void> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: source);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
+  double _calculateButtonWidth(String text, BuildContext context) {
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: text,
+        style: TextStyle(
+          fontSize: 14 * SizeConfig.textRatio,
+          fontFamily: "Roboto",
+        ),
+      ),
+      textDirection: TextDirection.ltr,
+    )..layout();
+
+    // Add some padding (20 on each side)
+    return textPainter.width + 40 * SizeConfig.horizontalBlock;
+  }
+
+  Future<void> _loadHandcrafterData() async {
+    try {
+      final handcrafter = await hvm.fetchHandcrafter();
+      setState(() {
+        _handcrafter = handcrafter;
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      print("Error loading customer data: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error loading profile data')),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHandcrafterData();
+    prodProvider = Provider.of<productProvider>(context, listen: false);
+    prodProvider.handCrafterProducts.clear();
+    prodProvider.fetchHandCrafter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        toolbarHeight: 308 * SizeConfig.verticalBlock,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF223F4A),
+                Color(0xFF5095B0),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
+        ),
+        leading: null,
+        automaticallyImplyLeading: false,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 10 * SizeConfig.verticalBlock,
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_back_ios_new,
+                    color: Colors.white,
+                    size: SizeConfig.textRatio * 15,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Text(
+                  'My Profile',
+                  style: TextStyle(
+                    fontFamily: "Rubik",
+                    fontSize: 20 * SizeConfig.textRatio,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            // Profile content below
+            Center(
+              child: Stack(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: SizeConfig.iconColor,
+                    radius: SizeConfig.horizontalBlock * 70,
+                    child: CircleAvatar(
+                      radius: SizeConfig.horizontalBlock * 67,
+                      backgroundColor: Colors.white,
+                      child: _handcrafter?.profileURL != null
+                          ? ClipOval(
+                        child: Image.network(
+                          _handcrafter!.profileURL!,
+                          width: SizeConfig.horizontalBlock * 134,
+                          height: SizeConfig.horizontalBlock * 134,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                          : Center(
+                        child: Icon(
+                          Icons.person,
+                          size: SizeConfig.horizontalBlock * 60,
+                          color: SizeConfig.iconColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                      ),
+                      child: CircleAvatar(
+                        backgroundColor: SizeConfig.iconColor,
+                        radius: SizeConfig.horizontalBlock * 20,
+                        child: IconButton(
+                          onPressed: () => _pickImage(ImageSource.gallery),
+                          icon: Icon(
+                            Icons.edit_outlined,
+                            color: Colors.white,
+                            size: SizeConfig.textRatio * 24,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Text(
+                _handcrafter?.name ?? 'No Name',
+                style: TextStyle(
+                  fontFamily: "Rubik",
+                  fontSize: 24 * SizeConfig.textRatio,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              _handcrafter?.description ?? 'NO Description',
+              style: TextStyle(
+                fontFamily: "Roboto",
+                fontSize: 16 * SizeConfig.textRatio,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          spacing: 15 * SizeConfig.verticalBlock,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: 15 * SizeConfig.verticalBlock,
+              ),
+              child: SizedBox(
+                height: 50 * SizeConfig.verticalBlock,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    SizedBox(width: 10 * SizeConfig.horizontalBlock),
+                    customizeButton(
+                      buttonName: "Add Product",
+                      buttonColor: Color(0xFF5095B0),
+                      fontColor: Color(0xFFFFFFFF),
+                      buttonIcon: Icons.add,
+                      IconColor: Color(0xFFFFFFFF),
+                      textSize: 14 * SizeConfig.textRatio,
+                      width: _calculateButtonWidth("Add Product", context),
+                      height: 40 * SizeConfig.verticalBlock,
+                    ),
+                    SizedBox(width: 10 * SizeConfig.horizontalBlock),
+                    customizeButton(
+                      buttonName: "Add Reel",
+                      buttonColor: Color(0xFFE9E9E9).withOpacity(0.5),
+                      fontColor: SizeConfig.iconColor,
+                      buttonIcon: Icons.add,
+                      IconColor: SizeConfig.iconColor,
+                      textSize: 14 * SizeConfig.textRatio,
+                      width: _calculateButtonWidth("Add Reel", context),
+                      height: 40 * SizeConfig.verticalBlock,
+                      onClickButton: () {
+                        Navigator.pushNamed(context, AddCrafterReel.id);
+                      },
+                    ),
+                    SizedBox(width: 10 * SizeConfig.horizontalBlock),
+                  ],
+                ),
+              ),
+            ),
+            Divider(
+              height: 2,
+              color: Color(0xFF3C3C3C).withOpacity(0.5),
+              indent: 70 * SizeConfig.horizontalBlock,
+              endIndent: 70 * SizeConfig.horizontalBlock,
+            ),
+            SizedBox(
+              height: 50 * SizeConfig.verticalBlock,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 10 * SizeConfig.horizontalBlock),
+                  customizeButton(
+                    buttonName: "Products",
+                    buttonColor: Color(0xFF5095B0),
+                    fontColor: Color(0xFFFFFFFF),
+                    textSize: 14 * SizeConfig.textRatio,
+                    width: _calculateButtonWidth("Products", context),
+                    height: 40 * SizeConfig.verticalBlock,
+                  ),
+                  SizedBox(width: 10 * SizeConfig.horizontalBlock),
+                  customizeButton(
+                    buttonName: "Reels",
+                    buttonColor: Color(0xFFE9E9E9).withOpacity(0.5),
+                    fontColor: SizeConfig.iconColor,
+                    textSize: 14 * SizeConfig.textRatio,
+                    width: _calculateButtonWidth("Reels", context),
+                    height: 40 * SizeConfig.verticalBlock,
+                  ),
+                  SizedBox(width: 10 * SizeConfig.horizontalBlock),
+                  customizeButton(
+                    buttonName: "Analysis",
+                    buttonColor: Color(0xFFE9E9E9).withOpacity(0.5),
+                    fontColor: SizeConfig.iconColor,
+                    textSize: 14 * SizeConfig.textRatio,
+                    width: _calculateButtonWidth("Analysis", context),
+                    height: 40 * SizeConfig.verticalBlock,
+                  ),
+                  SizedBox(width: 10 * SizeConfig.horizontalBlock),
+                  customizeButton(
+                    buttonName: "Orders",
+                    buttonColor: Color(0xFFE9E9E9).withOpacity(0.5),
+                    fontColor: SizeConfig.iconColor,
+                    textSize: 14 * SizeConfig.textRatio,
+                    width: _calculateButtonWidth("Orders", context),
+                    height: 40 * SizeConfig.verticalBlock,
+                  ),
+                  SizedBox(width: 10 * SizeConfig.horizontalBlock),
+                ],
+              ),
+            ),
+            Consumer<productProvider>(
+              builder: (context, prodProvider, child) {
+                if (prodProvider.handCrafterProducts.isEmpty) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return Padding(
+                  padding:
+                  const EdgeInsets.only(bottom: 8.0, right: 8, left: 8),
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    physics:
+                    const NeverScrollableScrollPhysics(), // Disable GridView's scrolling
+                    gridDelegate:
+                    const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, // Two products per row
+                      crossAxisSpacing: 10.0, // Spacing between columns
+                      mainAxisSpacing: 10.0, // Spacing between rows
+                      childAspectRatio: 0.7, // Adjust based on your design
+                    ),
+                    itemCount: prodProvider.handCrafterProducts.length,
+                    itemBuilder: (context, index) {
+                      var product = prodProvider.handCrafterProducts[index];
+                      return customProduct(
+                        product.imageURL,
+                        product.name,
+                        Category: product.category,
+                        product.price,
+                        product.rate,
+                        product.id,
+                        false,
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
